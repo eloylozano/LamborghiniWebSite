72b020f39490c320c3b3babce9b04ccfb458da2f
eloylozano <lozanobarrioseloy@gmail.com>
2023-12-27 22:53:13 +0100

    animación 1 header completada

--- scripts.js
+++ scripts.js
@@ -6,30 +6,28 @@ document.addEventListener("DOMContentLoaded", function () {
   var logo = document.getElementById("logo");
 
   modelos.addEventListener("mouseover", function () {
-    submenu.style.display = "block";
+    submenu.classList.add("show");
   });
 
-  header.addEventListener("mouseleave", function (event) {
-    // Comprobamos si el mouse sale del área del submenu y si no se encuentra sobre "beyond"
+  submenu.addEventListener("mouseleave", function (event) {
     if (
       !submenu.contains(event.relatedTarget) &&
       !beyond.contains(event.relatedTarget)
     ) {
-      submenu.style.display = "none";
+      submenu.classList.remove("show");
     }
   });
 
   beyond.addEventListener("mouseover", function () {
-    submenu.style.display = "none";
+    submenu.classList.remove("show");
   });
 
   logo.addEventListener("mouseover", function () {
-    submenu.style.display = "none";
+    submenu.classList.remove("show");
   });
 });
 
 function changeBackground(model, button) {
-  // Verifica si el botón ya tiene la clase "clicked"
   if (button.classList.contains("clicked")) {
     return;
   }
@@ -37,24 +35,19 @@ function changeBackground(model, button) {
   const configuradorMain = document.querySelector(".configurador-main");
   const hexagonEmpty = document.querySelector(".hexagon-empty");
 
-  // Restablece la clase clicked en todos los botones
   document.querySelectorAll(".buttons-configurador button").forEach((btn) => {
     btn.classList.remove("clicked");
   });
 
-  // Agrega la clase clicked al botón actual
   button.classList.add("clicked");
 
-  // Aplica la clase show al hexagon-empty para activar la animación
   hexagonEmpty.classList.add("show");
 
-  // Detén y reinicia la animación de los elementos h2
   const h2Letters = document.querySelectorAll(".text-configurador h2 div");
   h2Letters.forEach((letter) => {
     letter.classList.remove("show");
   });
 
-  // Cambia el fondo y el contenido según el modelo
   switch (model) {
     case "revuelto":
       configuradorMain.style.backgroundImage =
@@ -75,7 +68,6 @@ function changeBackground(model, button) {
       break;
   }
 
-  // Reinicia la animación de letra en letra después de un breve retraso
   setTimeout(() => {
     h2Letters.forEach((letter, index) => {
       setTimeout(() => {
@@ -86,13 +78,11 @@ function changeBackground(model, button) {
 }
 
 function updateH1Content(content) {
-  // Elimina la clase show de los h1
   const h1Elements = document.querySelectorAll(".text-configurador h1");
   h1Elements.forEach((h1) => {
     h1.classList.remove("show");
   });
 
-  // Añade la clase show a los h1 con un retraso y actualiza el contenido
   setTimeout(() => {
     h1Elements.forEach((h1) => {
       h1.textContent = `CREAR TU ${content}`;
@@ -102,7 +92,6 @@ function updateH1Content(content) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Añade la clase show al hexagon-empty al cargar la página con un retraso
   const hexagonEmpty = document.querySelector(".hexagon-empty");
   setTimeout(() => {
     hexagonEmpty.classList.add("show");
